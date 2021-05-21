49c52563c7ded10a7a223a1b1f00203a2dbe7382
Suhassura <matrixsssuhas@gmail.com>
2021-05-21 19:53:06 +0530

    Add: Hilt DI improvements

--- app/src/main/java/com/compose/panthercompose/moviesappmvi/data/repository/MovieRepository.kt
+++ app/src/main/java/com/compose/panthercompose/moviesappmvi/data/repository/MovieRepository.kt
@@ -21,6 +21,7 @@ class MovieRepository @Inject constructor(
     private val genreDao: GenreDao
 ) {
 
+
     fun getNowShowing(): Flow<List<Movie>> = flow {
         val response = movieApi.getMovies(1)
         if (response.isSuccessful) {
